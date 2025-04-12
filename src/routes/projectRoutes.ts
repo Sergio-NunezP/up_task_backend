f9ed1bb38ad10e78b2b096564dda16a631cf096a
Sergio Núñez <sergioop722@gmail.com>
2025-04-12 11:45:34 -0500

    Agrgando validación en el servidor a los proyectos

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
+import { handleInputError } from '../middleware/validation';
 
 const router = Router();
 
 // Crear un proyecto : POST /api/projects
-router.post('/', ProjectController.createProject);
+router.post('/',
+    body('projectName')
+        .notEmpty().withMessage('El Nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El Nombre del Cliente es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La description del proyecto es obligatorio'),
+    handleInputError,
+    ProjectController.createProject);
+
 // Obtener todos los proyectos : GET /api/projects
 router.get('/', ProjectController.getAllProjects);
 export default router;
