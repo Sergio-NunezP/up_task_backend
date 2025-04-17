d10a8dc7ecb826f2f6563be9006306ae75314df9
Sergio Núñez <sergioop722@gmail.com>
2025-04-17 10:37:24 -0500

    obtener las tareas de un proyecto

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -28,7 +28,7 @@ export class ProjectController {
     static getProjectByID = async (req: Request, res: Response) => {
         const { id } = req.params
         try {
-            const project = await Project.findById(id)
+            const project = await Project.findById(id).populate('tasks')
             if (!project) {
                 const error = new Error('Proyecto no encontrado')
                 res.status(404).json({ error: error.message })
@@ -60,7 +60,7 @@ export class ProjectController {
     static deleteProject = async (req: Request, res: Response) => {
         const { id } = req.params
         try {
-            const project = await Project.findById(id)
+            const project = await (await Project.findById(id))
             if (!project) {
                 const error = new Error('Proyecto no encontrado')
                 res.status(404).json({ error: error.message })
